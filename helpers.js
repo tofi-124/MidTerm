3824aa69ffe7bef75e34d4fa8fa3b30840f095a7
EvilDoll69 <tetiana.fediushyn@gmail.com>
2022-01-10 21:59:39 +0000

    Refactor routes

--- helpers.js
+++ helpers.js
@@ -21,4 +21,22 @@ function passwordFinder(users, email, password) {
   }
 }
 
-module.exports = { getUserByEmail, passwordFinder };
+const loginCheck = function (email, password, db) {
+  return db
+    .query(`SELECT * FROM users WHERE email = $1;`, [email])
+    .then((res) => {
+      if (res.rows) {
+        return res.rows[0];
+      }
+      return null;
+    })
+    .then((user) => {
+      if (bcrypt.compareSync(password, user.password)) {
+        return user;
+      }
+      return null;
+    });
+};
+
+
+module.exports = { getUserByEmail, passwordFinder, loginCheck };
