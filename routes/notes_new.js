d92420addea9a99c4916e249dfc5e4327b45715a
Tofik Mohammed <tofikahm@yahoo.com>
2022-01-10 21:10:39 +0000

    adding post route

--- routes/notes_new.js
+++ routes/notes_new.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    if (!req.session.user_id) {
+      res.redirect("/home");
+      return;
+    }
+    const templateVars = {
+      user_id: req.session.user_id,
+    };
+    res.render("notes_new", templateVars);
+  });
 
-
-
-
+  
