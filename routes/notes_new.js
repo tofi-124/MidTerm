d1b64b8c9c9794fc741ee94293b9ca25a4666a21
Tofik Mohammed <tofikahm@yahoo.com>
2022-01-10 19:52:42 +0000

    modfied so that it registers the user id in the urls database

--- routes/notes_new.js
+++ routes/notes_new.js
@@ -15,10 +15,11 @@ module.exports = (db) => {
 
   router.post("/", (req, res) => {
     const urls = req.body;
+    let user_id = req.session.user_id;
   
     db.query(
-      `INSERT INTO urls (title, topic, url, description) VALUES ($1, $2, $3, $4) RETURNING *;`,
-      [urls.title, urls.topic, urls.url, urls.description]
+      `INSERT INTO urls (title, topic, url, description, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
+      [urls.title, urls.topic, urls.url, urls.description, user_id]
     )
       .then((res) => res.rows[0])
       .then((urls) => {
