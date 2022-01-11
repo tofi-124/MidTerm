e719ab9b5aefcff65ae9abe1685d4f9fe781c91e
EvilDoll69 <tetiana.fediushyn@gmail.com>
2022-01-11 19:41:15 +0000

    Create My resources page

--- routes/NotesRouter.js
+++ routes/NotesRouter.js
@@ -5,6 +5,8 @@ module.exports = (db) => {
   router.get("/hello", (req, res) => {
     res.send("HELLO FROM NOTES!");
   });
+
+
   router.post("/:id/edit", (req, res) => {});
   router.post("/:id/delete", (req, res) => {});
 
