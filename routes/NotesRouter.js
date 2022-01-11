1aab0ce05b1eef8e2332a025287a27a9bb8eec45
Tofik Mohammed <tofikahm@yahoo.com>
2022-01-11 05:46:35 +0000

    removed non-working routes

--- routes/NotesRouter.js
+++ routes/NotesRouter.js
@@ -33,29 +33,9 @@ module.exports = (db) => {
     }
   });
 
-  router.post("/:id/edit", (req, res) => {
-    if (!req.session.user_id) {
-      res.redirect("/register");
-      return;
-    }
-    // this part has problem, need ask mentor
-    console.log(req.body);
-    db.query(
-      `INSERT INTO URLs (title, url, description) VALUES ($1, $2, $3) RETURNING *`
-    );
-  });
+  router.post("/:id/edit", (req, res) => {});
 
-  router.post("/:id/delete", (req, res) => {
-    if (!req.session.user_id) {
-      res.redirect("/register");
-      return;
-    }
-    // this part has problem, need ask mentor
-    console.log(req.body);
-    db.query(
-      `INSERT INTO URLs (title, url, description) VALUES ($1, $2, $3) RETURNING *`
-    );
-  });
+  router.post("/:id/delete", (req, res) => {});
 
   return router;
 };
