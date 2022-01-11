c2bb5b943346d3b2c954ed96fa63a2fb53166f36
NickCoconut <gelaxyy@gmail.com>
2022-01-11 04:13:39 +0000

    change Routes

--- server.js
+++ server.js
@@ -1,28 +1,56 @@
-// load .env data into process.env
-require("dotenv").config();
+//------------------------------------- REQUIREMENTS IN APPLICATION ----------------------------//
 
-// Web server config
-const PORT = process.env.PORT || 8080;
-const sassMiddleware = require("./lib/sass-middleware");
+require("dotenv").config(); // load .env data into process.env
+
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const { Pool } = require("pg");
+const cookieSession = require("cookie-session");
+const bcrypt = require("bcryptjs");
+
+const dbParams = require("./lib/db.js");
+const sassMiddleware = require("./lib/sass-middleware");
+const {
+  getUserByEmail,
+  passwordFinder,
+} = require("./helpers");
+
+
+// Separated Routes for each Resource
+// Note: Feel free to replace the example routes below with your own
+// const usersRoutes = require("./routes/users");
+// const widgetsRoutes = require("./routes/widgets");
+
+const routes = require('./routes');
+
+
+//------------------------------------- SERVER SETTINGS AND MIDLWARES IN APPLICATION ----------------------------//
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
 
 // PG database client/connection setup
-const { Pool } = require("pg");
-const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 db.connect(() => {
   console.log('connected to database')
 });
 
+app.set("view engine", "ejs");
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan("dev"));
+app.use(morgan("dev")); //just a logger
 
-app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key"],
+  })
+);
 
 app.use(
   "/styles",
@@ -35,66 +63,26 @@ app.use(
 
 app.use(express.static("public"));
 
-//-----------------------------------------------------------------//
-//---------------------------->>> NPMS <<<-------------------------//
-//-----------------------------------------------------------------//
-
-// Setting up cookie session
-// http://expressjs.com/en/resources/middleware/cookie-session.html
-let cookieSession = require("cookie-session");
-app.use(
-  cookieSession({
-    name: "session",
-    keys: ["key"],
-  })
-);
-
-// Body parser
-// https://www.npmjs.com/package/body-parser
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Bcrypt
-// https://www.npmjs.com/package/bcrypt
-const bcrypt = require("bcryptjs");
-
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-// const usersRoutes = require("./routes/users");
-// const widgetsRoutes = require("./routes/widgets");
-
-const homePage = require("./routes/home")
-const registerPage = require("./routes/register");
-const loginPage = require("./routes/login");
-const logoutPage = require("./routes/logout");
-const notes_new = require("./routes/notes_new");
-const notes = require("./routes/notes");
-
+//------------------------------------- ROUTES/ENDPOINTS  ----------------------------//
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // app.use("/api/users", usersRoutes(db));
 // app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
-app.use("/home", homePage());
-app.use("/register", registerPage(db));
-app.use("/login", loginPage(db));
-app.use("/logout", logoutPage());
-app.use("/notes", notes(db));
-app.use("/notes/new", notes_new(db));
+app.use(routes(db));
 
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
 
-app.get("/index", (req, res) => {
-  res.render("index");
-});
-
+//------------------------------------- LISTENER  ------------------------------------//
 //Start initialization
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-// home
-app.get("/", (req, res) => res.redirect("/home"));
+
+
+
+
+
+
+
 
