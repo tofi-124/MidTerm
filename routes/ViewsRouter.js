d93ac7670dda0999b83191d98682e974da06e0c5
Tofik Mohammed <tofikahm@yahoo.com>
2022-01-12 08:34:05 +0000

    added routes for comments and ratings

--- routes/ViewsRouter.js
+++ routes/ViewsRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 module.exports = (db) => {
   router.get("/hello", (req, res) => {
     res.send("HELLO FROM VIEWS!");
-  })
+  });
 
   //-------------------VIEW HOMEPAGE ----------------------//
   router.get("/", (req, res) => {
@@ -13,9 +13,9 @@ module.exports = (db) => {
       return res.redirect("/notes");
     }
     const templateVars = {
-      user_id: null
+      user_id: null,
     };
-    return res.render("index", templateVars)
+    return res.render("index", templateVars);
   });
 
   //-------------------VIEW AUTHROUTES ----------------------//
@@ -25,9 +25,9 @@ module.exports = (db) => {
       return res.redirect("/notes");
     }
     const templateVars = {
-      user_id: null
+      user_id: null,
     };
-    return res.render("register", templateVars)
+    return res.render("register", templateVars);
   });
 
   router.get("/login", (req, res) => {
@@ -36,9 +36,9 @@ module.exports = (db) => {
       return res.redirect("/notes");
     }
     const templateVars = {
-      user_id: null
+      user_id: null,
     };
-    return res.render("login", templateVars)
+    return res.render("login", templateVars);
   });
 
   //-------------------VIEW NOTES ROUTES ----------------------//
@@ -50,7 +50,9 @@ module.exports = (db) => {
     }
 
     try {
-      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [user_id]) //checking id from the db
+      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [
+        user_id,
+      ]); //checking id from the db
       if (!validUser) {
         return res.redirect("/");
       }
@@ -58,11 +60,11 @@ module.exports = (db) => {
       const resources = await db.query(`SELECT * FROM URLs;`);
       const templateVars = {
         user_id: validUser.rows[0].name,
-        notes: resources.rows
-      }
+        notes: resources.rows,
+      };
       return res.render("notes_index", templateVars);
     } catch (error) {
-      return res.status(400).send({message: error.message});
+      return res.status(400).send({ message: error.message });
     }
   });
 
@@ -73,19 +75,21 @@ module.exports = (db) => {
     }
 
     try {
-      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [user_id]) //checking id from the db
+      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [
+        user_id,
+      ]); //checking id from the db
       if (!validUser) {
         return res.redirect("/");
       }
 
       const templateVars = {
-        user_id: validUser.rows[0].name
-      }
+        user_id: validUser.rows[0].name,
+      };
       return res.render("notes_new", templateVars);
     } catch (error) {
-      return res.status(400).send({message: error.message});
+      return res.status(400).send({ message: error.message });
     }
-  })
+  });
 
   router.get("/notes/myresources", async (req, res) => {
     const { user_id } = req.session; // checking cookies
@@ -94,21 +98,25 @@ module.exports = (db) => {
     }
 
     try {
-      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [user_id]) //checking id from the db
+      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [
+        user_id,
+      ]); //checking id from the db
       if (!validUser) {
         return res.redirect("/");
       }
 
-      const resources = await db.query(`SELECT * FROM URLs WHERE user_id = $1;`, [user_id]);
+      const resources = await db.query(
+        `SELECT * FROM URLs WHERE user_id = $1;`,
+        [user_id]
+      );
       const templateVars = {
         user_id: validUser.rows[0].name,
-        notes: resources.rows
-      }
+        notes: resources.rows,
+      };
       return res.render("notes_myresources", templateVars);
     } catch (error) {
-      return res.status(400).send({message: error.message});
+      return res.status(400).send({ message: error.message });
     }
-
   });
 
   router.get("/topic", async (req, res) => {
@@ -118,7 +126,9 @@ module.exports = (db) => {
     }
 
     try {
-      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [user_id]) //checking id from the db
+      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [
+        user_id,
+      ]); //checking id from the db
       if (!validUser) {
         return res.redirect("/");
       }
@@ -126,47 +136,130 @@ module.exports = (db) => {
       const resources = await db.query(`SELECT * FROM URLs;`);
       const templateVars = {
         user_id: validUser.rows[0].name,
-        notes: resources.rows
-      }
+        notes: resources.rows,
+      };
       return res.render("topic", templateVars);
     } catch (error) {
-      return res.status(400).send({message: error.message});
+      return res.status(400).send({ message: error.message });
     }
   });
 
-  router.post("/topic", async (req,res)=>{
+  router.post("/topic", async (req, res) => {
     const { user_id } = req.session; // checking cookies
     if (!user_id) {
       return res.redirect("/");
     }
 
     try {
-      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [user_id]) //checking id from the db
+      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [
+        user_id,
+      ]); //checking id from the db
       if (!validUser) {
         return res.redirect("/");
       }
 
-      const {topic} = req.body;
-      const resources = await db.query(`SELECT * FROM URLs WHERE topic = $1;`,[topic]);
+      const { topic } = req.body;
+      const resources = await db.query(`SELECT * FROM URLs WHERE topic = $1;`, [
+        topic,
+      ]);
       const templateVars = {
         user_id: validUser.rows[0].name,
-        notes: resources.rows
-      }
+        notes: resources.rows,
+      };
       return res.render("topic", templateVars);
     } catch (error) {
-      return res.status(400).send({message: error.message});
+      return res.status(400).send({ message: error.message });
     }
-  })
+  });
 
-  
+  router.get("/comment", async (req, res) => {
+    const { user_id } = req.session; // checking cookies
+    if (!user_id) {
+      return res.redirect("/");
+    }
+
+    try {
+      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [
+        user_id,
+      ]); //checking id from the db
+      if (!validUser) {
+        return res.redirect("/");
+      }
+
+      return res.redirect("/notes");
+    } catch (error) {
+      return res.status(400).send({ message: error.message });
+    }
+  });
+
+  router.post("/comment", async (req, res) => {
+    const { user_id } = req.session; // checking cookies
+    if (!user_id) {
+      return res.redirect("/");
+    }
+
+    try {
+      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [
+        user_id,
+      ]); //checking id from the db
+      if (!validUser) {
+        return res.redirect("/");
+      }
+      console.log(req.body)
+
+      return res.redirect("/notes");
+    } catch (error) {
+      return res.status(400).send({ message: error.message });
+    }
+  });
+
+  router.get("/rating", async (req, res) => {
+    const { user_id } = req.session; // checking cookies
+    if (!user_id) {
+      return res.redirect("/");
+    }
+
+    try {
+      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [
+        user_id,
+      ]); //checking id from the db
+      if (!validUser) {
+        return res.redirect("/");
+      }
+
+      return res.redirect("/notes");
+    } catch (error) {
+      return res.status(400).send({ message: error.message });
+    }
+  });
+
+  router.post("/rating", async (req, res) => {
+    const { user_id } = req.session; // checking cookies
+    if (!user_id) {
+      return res.redirect("/");
+    }
+
+    try {
+      const validUser = await db.query(`SELECT * FROM users WHERE id = $1;`, [
+        user_id,
+      ]); //checking id from the db
+      if (!validUser) {
+        return res.redirect("/");
+      }
+      console.log(req.body)
+
+      return res.redirect("/notes");
+    } catch (error) {
+      return res.status(400).send({ message: error.message });
+    }
+  });
 
   return router;
 };
 
-
 //-----------Query to search by the topic-------------//
 //db.query(
-  //     `SELECT title, url, description, rating, comment
-  //   FROM URLs JOIN url_ratings ON URLs.id = url_id
-  //   WHERE topic = $1;`, [topic]
-  //   )
+//     `SELECT title, url, description, rating, comment
+//   FROM URLs JOIN url_ratings ON URLs.id = url_id
+//   WHERE topic = $1;`, [topic]
+//   )
